3b666ef4b87b049214760a692fb0082aa6f0d325
avifro-dev <avi.froelich@logz.io>
2022-12-04 17:10:13 +0200

    Fix java.lang.UnsupportedOperationException: JsonNull in percentile aggs

--- jest-common/src/main/java/io/searchbox/core/search/aggregation/PercentilesAggregation.java
+++ jest-common/src/main/java/io/searchbox/core/search/aggregation/PercentilesAggregation.java
@@ -1,6 +1,7 @@
 package io.searchbox.core.search.aggregation;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 
 import java.util.HashMap;
@@ -23,7 +24,8 @@ public class PercentilesAggregation extends MetricAggregation {
 
     private void parseSource(JsonObject source) {
         for (Map.Entry<String, JsonElement> entry : source.entrySet()) {
-            if(!(Double.isNaN(entry.getValue().getAsDouble()))) {
+            // In case of no results in Elasticsearch the behaviour was to get 'NaN' and in OpenSearch is 'null'
+            if(!entry.getValue().equals(JsonNull.INSTANCE) && !(Double.isNaN(entry.getValue().getAsDouble()))) {
                 percentiles.put(entry.getKey(), entry.getValue().getAsDouble());
             }
         }
